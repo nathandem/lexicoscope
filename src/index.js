dd38501e3d930fae12bc05392decb45f0505d4d0
Nathan <nathan.demaestri@gmail.com>
2019-09-21 14:38:33 +0200

    Move global style from index.html to index.js
    No style remains in the index.html, unprocessed. Everything go
    through webpack processing.

--- src/index.js
+++ src/index.js
@@ -7,5 +7,9 @@ import 'normalize.css/normalize.css';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 
+// global custom style
+import './style/global.css';
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
