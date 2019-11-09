082fe6189ed97a72f58643e20fbe6ad9457ba031
Nathan <nathan.demaestri@gmail.com>
2019-11-09 12:14:31 +0100

    Adjust layout style a bit

--- src/App.js
+++ src/App.js
@@ -7,22 +7,22 @@ import Footer from './Footer';
 import Home from './Home';
 import Help from './Help';
 import Analytics from './analytics/Analytics';
+import Results from './analytics/results/Results';
 
 
 function App() {
   return (
-    <>
-    <Navbar/>
     <div className="wrapper">
+      <Navbar/>
       <div className="layout">
         {/* Insert the pages here -> react rooter */}
         <Route exact path="/" component={Home} />
         <Route path="/analytics" component={Analytics} />
+        <Route path="/results" component={Results} />
         <Route path="/help" component={Help} />
         <Footer />
       </div>
     </div>
-  </>
   );
 }
 
