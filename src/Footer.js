669f6d79b43fd56bb20a44377db8a7fae9e58a60
ndem <nathan.demaestri@gmail.com>
2019-12-07 11:41:14 +0100

    Add a decent margin-top to the footer everywhere

--- src/Footer.js
+++ src/Footer.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
-import './style/Footer.css';
-
 
 export default function Footer () {
 
     const currentYear = new Date().getFullYear();
 
     return (
-        <footer className="bp3-text-small Footer_footer center-text margin-bottom-1-5-rem">
+        <footer className="bp3-text-small center-text margin-top-3-rem padding-bottom-1-rem">
             Copyright © 2016-{currentYear} Université Grenoble Alpes - Crédits
         </footer>
     );
