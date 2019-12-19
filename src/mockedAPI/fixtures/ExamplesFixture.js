75ef8d833dafe640cbeb485e3a66496549e61555
ndem <nathan.demaestri@gmail.com>
2019-12-19 08:19:44 +0100

    Update mocked responses to reflect new behaviors

--- src/mockedAPI/fixtures/ExamplesFixture.js
+++ src/mockedAPI/fixtures/ExamplesFixture.js
@@ -1,18 +1,13 @@
 export const ExamplesFixture = {
    "lang" : "fr",
+   "session_time" : "19/11/0119 - 07:25:26",
+   "session_id" : null,
    "examples" : [
       {
-         "freq" : 247,
-         "query" : "<l=voiture,c=NOUN,#1>",
-         "example" : "L'explosion de la  [[[voiture#1]]]  qui le tua, lui et son escorte, à Tachkent, ne fut jamais revendiquée, mais Shabazz était persuadé que c'était Hakmad et ce qui allait devenir le FLNO qui avaient commandité, voire exécuté eux-mêmes le coup.\\n     ",
-         "svg" : "<svg width=\"138pt\" height=\"44pt\"\n viewBox=\"0.00 0.00 138.00 44.00\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">\n<g id=\"graph0\" class=\"graph\" transform=\"scale(1 1) rotate(0) translate(4 40)\">\n<title>Dependencies</title>\n<polygon fill=\"white\" stroke=\"none\" points=\"-4,4 -4,-40 134,-40 134,4 -4,4\"/>\n<!-- 1 -->\n<g id=\"node1\" class=\"node\"><title>1</title>\n<ellipse fill=\"none\" stroke=\"black\" cx=\"65\" cy=\"-18\" rx=\"65.4659\" ry=\"18\"/>\n<text text-anchor=\"middle\" x=\"65\" y=\"-14.3\" font-family=\"Times,serif\" font-size=\"14.00\">voiture_NOUN</text>\n</g>\n</g>\n</svg>"
-      },
-      {
-         "example" : "Une seconde  [[[voiture#1]]]  – une Frazer verte – prenait le même chemin.\\n     ",
-         "svg" : "<svg width=\"140pt\" height=\"44pt\"\n viewBox=\"0.00 0.00 140.00 44.00\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">\n<g id=\"graph0\" class=\"graph\" transform=\"scale(1 1) rotate(0) translate(4 40)\">\n<title>Dependencies</title>\n<polygon fill=\"white\" stroke=\"none\" points=\"-4,4 -4,-40 136,-40 136,4 -4,4\"/>\n<!-- 1 -->\n<g id=\"node1\" class=\"node\"><title>1</title>\n<ellipse fill=\"none\" stroke=\"black\" cx=\"66\" cy=\"-18\" rx=\"65.9423\" ry=\"18\"/>\n<text text-anchor=\"middle\" x=\"66\" y=\"-14.3\" font-family=\"Times,serif\" font-size=\"14.00\">voiturer_VERB</text>\n</g>\n</g>\n</svg>",
-         "freq" : 7,
-         "query" : "<l=voiturer,c=VERB,#1>"
+         "svg" : "<svg width=\"130pt\" height=\"44pt\"\n viewBox=\"0.00 0.00 130.00 44.00\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">\n<g id=\"graph0\" class=\"graph\" transform=\"scale(1 1) rotate(0) translate(4 40)\">\n<title>Dependencies</title>\n<polygon fill=\"white\" stroke=\"none\" points=\"-4,4 -4,-40 126,-40 126,4 -4,4\"/>\n<!-- 1 -->\n<g id=\"node1\" class=\"node\"><title>1</title>\n<ellipse fill=\"none\" stroke=\"black\" cx=\"61\" cy=\"-18\" rx=\"61.1419\" ry=\"18\"/>\n<text text-anchor=\"middle\" x=\"61\" y=\"-14.3\" font-family=\"Times,serif\" font-size=\"14.00\">crime_NOUN</text>\n</g>\n</g>\n</svg>",
+         "query" : "<l=crime,c=NOUN,#1>",
+         "example" : "Ils s'enfuirent tous les deux et vécurent heureux jusqu'à la fin de leur vie, mais toute la population paya pour leur  [[[crime#1]]] , et la désolation frappa le pays. ",
+         "freq" : 2226
       }
-   ],
-   "session_time" : "05/11/0119 - 20:52:55"
+   ]
 };
