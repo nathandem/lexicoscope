b93d210e78267970d8579e62751013c7510b192b
ndem <nathan.demaestri@gmail.com>
2019-12-28 22:40:25 +0100

    Remove (now) unneeded comment

--- src/app/analytics/corpus/choice/TypesChoice.js
+++ src/app/analytics/corpus/choice/TypesChoice.js
@@ -6,9 +6,6 @@ import { H1 } from '@blueprintjs/core';
 import TypeChoice from './TypeChoice';
 import { CORPUS_TYPES } from '../constants';
 
-// this deeply nested import is the price to pay to stay in the default
-// configuration of create react app, and be sure my override of the default
-// won't break anything related to the compilation chain (webpack, babel, etc)
 import './TypesChoice.css';
 
 
