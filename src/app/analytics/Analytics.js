63c8726ba42dc1bc2c59e933db3ec580e570d483
ndem <nathan.demaestri@gmail.com>
2019-12-28 15:03:11 +0100

    Open search results in a new tab

--- src/app/analytics/Analytics.js
+++ src/app/analytics/Analytics.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Corpus from './corpus/Corpus.js';
 import Search from './search/Search.js';
 import Results from './results/Results.js';
+import { ANALYTICS_URL } from '../constants';
 
 
 export default class Analytics extends React.Component {
@@ -16,7 +17,61 @@ export default class Analytics extends React.Component {
     page: 'corpus',  // 3 options: 'corpus', 'query', 'results'
   };
 
-  performSearch = () => {
+  componentDidMount() {
+    this.parseQueryParams();
+  }
+
+  parseQueryParams = () => {
+    // Analytics can be opened in 3 cases:
+    // 1. from within the app, e.g from the home page: no queryparams
+    //    -> show type choose page
+    // 2. from a new tab, after a query is made: session_id in queryparams
+    //    -> run the query and show the results
+    // 3. from Sascha's app: query, collection, lang, category in queryparams
+    //    -> run the query and show the results
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const session_id = urlParams.get('session_id');
+    const query = urlParams.get('query');
+    const collection = urlParams.get('collection');
+    const lang = urlParams.get('lang');
+    const category = urlParams.get('category');
+
+    // deal with queries from the Lexicoscope
+    if (session_id) {
+      const queryArgs = { session_id };
+      this.performSearch(queryArgs);
+    }
+
+    // deal with queries from Sascha's app
+    else if (query && collection && lang && category) {
+      const queryArgs = { query, collection, lang, category };
+      this.performSearch(queryArgs);
+    }
+  }
+
+  performSearch = (queryArgs) => {
+    const endpoint = '/search.ajax.php';
+    const queryParams = new URLSearchParams(queryArgs);
+    fetch(
+      process.env.REACT_APP_API_BASE + endpoint + '?' + queryParams, {
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+        method: 'GET',
+      }
+    )
+    .then((res) => {
+      if (res.status !== 200) {
+        console.log(`Issue reaching ${endpoint}`);
+        return;
+      }
+      res.json().then((data) => {
+        this.setState({ results: data, page: 'results' });
+      });
+    })
+  }
+
+  openSearchInNewTab = () => {
 
     const data = {
       query: this.state.query,
@@ -41,30 +96,14 @@ export default class Analytics extends React.Component {
         return;
       }
       res.json().then((data) => {
-        // No need to store the session_id in the state, as create_session_id
-        // is called every time. If the session already exists, the same
-        // gets session_id sent back.
-        // Note: this session is a query session, not a user session (the one
+        // This session is a query session, not a user session (the one
         // tracked by the session cookie).
         const sessionId = data.session_id;
-
-        const endpoint = '/search.ajax.php';
         const queryParams = new URLSearchParams({ session_id: sessionId });
-        fetch(
-          process.env.REACT_APP_API_BASE + endpoint + '?' + queryParams, {
-            credentials: 'include',
-            headers: { 'Content-Type': 'application/json' },
-          }
-        )
-        .then((res) => {
-          if (res.status !== 200) {
-            console.log(`Issue reaching ${endpoint}`);
-            return;
-          }
-          res.json().then((data) => {
-            this.setState({ results: data, page: 'results' });
-          });
-        })
+
+        // this url must reflect the one defined in `App` for `Analytics`
+        const url = ANALYTICS_URL + '?' + queryParams;
+        window.open(url, '_blank');
       })
     })
     .catch(() => {
@@ -78,7 +117,7 @@ export default class Analytics extends React.Component {
 
   handleOnQueryReady = (query) => {
     this.setState({ query }, () => {
-      this.performSearch();
+      this.openSearchInNewTab();
     });
   }
 
